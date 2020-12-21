a1aaf0b01247d5d573e3c12cc3ced69144d23984
janodetzel <jano.detzel@gmail.com>
2020-12-22 00:04:52 +0100

    day7 silver

---
+++ 07/script02.js
@@ -0,0 +1,33 @@
+let fs = require("fs");
+
+const input = fs
+    .readFileSync("./input.txt")
+    .toString()
+    .split(/\r\n|\n|\r/);
+
+
+const bags = new Set([])
+
+const findThis = "shiny gold"
+
+const countBags = bagColor => {
+
+    input.find(rule => {
+        let [containing, contains] = rule.split("contain")
+
+        containing = containing.split("bags")[0]
+
+
+        if (containing.includes(bagColor)) {
+
+            let [x, number, bag] = contains.split(/(\d+)/)
+
+            bags.add(containing)
+            countBags(containing)
+        }
+    })
+
+}
+
+countBags(findThis)
+console.log(`${bags.size}`)
